24b4b78f198836b3c795c7acb72ecbb2e55e1f8e
thanksdanny <hj-07@163.com>
2016-03-06 00:28:23 +0800

    Initial Commit

---
+++ SpotifyVideoBackground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotifyVideoBackground
+//
+//  Created by Danny Ho on 3/6/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
